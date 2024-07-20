183bc4401ac7f5588ec6dbec70ea7fa517293fd9
Assoghe <soraya.gherras17@gmail.com>
2024-07-20 03:28:31 +0200

    Add margin

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -5,8 +5,8 @@ import { Herosection } from './sections/herosection/HeroSection'
 function App() {
 
   return (
-    <div className='my-0 py-0'>
-     <Herosection/>
+    <div className='my-0'>
+      <Herosection />
     </div>
   )
 }
--- landing/src/sections/herosection/HeroSection.tsx
+++ landing/src/sections/herosection/HeroSection.tsx
@@ -3,8 +3,8 @@ import { Typography } from "../../design system/Typography/Typography"
 export const Herosection = () => {
     return (
         <div className="bg-cover h-screen bg-[url('./assets/img/herosection.png')]">
-            <div className="h-screen w-7/12 flex flex-col justify-center">
-                <span>Livre blanc</span>
+            <div className="h-screen w-6/12 flex flex-col justify-center mx-64">
+                <span className="gap-5">Livre blanc</span>
                 <Typography variant='h1' component='h1' className='text-neutral-100'>
                     Intégrer efficacement une maquette web tout en affrontant les défis liés au développement web
                 </Typography>
