df214bece64a235ebf82113b481bda32333768cb
Assoghe <soraya.gherras17@gmail.com>
2024-07-20 21:51:45 +0200

    DesignDevSection > Add img and padding

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { AboutTheBook } from './sections/AboutTheBook/AboutTheBook'
+import { DesignDevSection } from './sections/DesignDevSection/DesignDevSection'
 import { ForWhoSection } from './sections/ForWhoSection/ForWhoSection'
 import { FindSection } from './sections/WhatWillYouFindSection/FindSection'
 import { Herosection } from './sections/herosection/HeroSection'
@@ -13,6 +14,7 @@ function App() {
       <AboutTheBook/>
       <ForWhoSection/>
       <FindSection/>
+      <DesignDevSection/>
     </div>
   )
 }
