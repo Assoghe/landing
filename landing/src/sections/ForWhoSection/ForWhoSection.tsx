c33984bfd43f45f875c32b6e390edfa43fca81f4
Assoghe <soraya.gherras17@gmail.com>
2024-07-20 05:16:37 +0200

    ForWhoSection > Add BlockIcons

--- landing/src/sections/ForWhoSection/ForWhoSection.tsx
+++ landing/src/sections/ForWhoSection/ForWhoSection.tsx
@@ -1,11 +1,13 @@
+import IconBlock from '../../design system/BlockIcon/BlockIcon'
 import { Typography } from '../../design system/Typography/Typography'
 import { Button } from '../../design system/button/Button'
+import { HiAcademicCap, HiCodeBracket, HiPencil, HiPresentationChartLine } from 'react-icons/hi2'
 
 export const ForWhoSection = () => {
     return (
         <div className='mx-6 tablet:mx-64'>
-            <div className='tablet:flex'>
-                <Typography variant='h2' component='h2'className='w-[550px] mr-9 mb-6'>
+            <div className='tablet:flex tablet:mb-24 mb-9'>
+                <Typography variant='h2' component='h2' className='w-[550px] mr-9 mb-6'>
                     Ce livre blanc est pour ...
                 </Typography>
                 <div>
@@ -16,6 +18,25 @@ export const ForWhoSection = () => {
                 </div>
             </div>
 
+            <div className='flex gap-9'>
+                <div className="flex justify-center items-center w-full">
+                    <IconBlock icon={HiPencil} title="Designers" />
+                </div>
+
+                <div className="flex justify-center items-center w-full">
+                    <IconBlock icon={HiCodeBracket} title="Développeurs" />
+                </div>
+
+                <div className="flex justify-center items-center w-full">
+                    <IconBlock icon={HiPresentationChartLine} title="Chefs de projet" />
+                </div>
+
+                <div className="flex justify-center items-center w-full">
+                    <IconBlock icon={HiAcademicCap} title="Etudiants" />
+                </div>
+            </div>
         </div>
+
+
     )
 }
